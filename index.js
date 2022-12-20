1417b82d25ce1df4630fde4aaa38af31a5b33d2c
Luis cespedes <lxluxo23@gmail.com>
2022-12-20 00:24:00 -0300

    funcionando de pana

--- index.js
+++ index.js
@@ -1,85 +1,83 @@
 require('dotenv').config()
-const { Client, GatewayIntentBits, REST } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
+const ytdl = require('ytdl-core');
+const fs = require("fs");
+var prefix = '!pi'
+const { Client, GatewayIntentBits, REST, ActivityType } = require('discord.js');
+const { AudioPlayerStatus } = require('@discordjs/voice');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource
+} = require('@discordjs/voice');
 const token = process.env.TOKEN
 client.on('ready', () => {
-    console.log(`Logueado en el bot csm  ${client.user.tag}!`)
+    console.log(`Logueado en el bot  ${client.user.tag}!`)
+    client.user.setActivity('pepillo GOD', { type: ActivityType.Playing });
+
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const { commandName } = interaction;
 
-    if (commandName === 'pipazo') {
-        console.log("comando pipazo")
-        interaction.reply('pelmazo ql');
-        try {
-            for (i = 0; i < 10;) {
-                setTimeout(() => {
-                    client.users.fetch('385682426829209620', false).then((user) => {
-                        user.send('pelmazo ql');
-                    });
-                }, 2000);
-                i++
-            }
+    // algun dia llenare esto
 
 
-        } catch (error) {
-            console.error("error en el comando ")
-        }
+});
+client.on('messageCreate', async (message) => {
+
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return
     }
-    if (commandName === 'yo') {
-        console.log("prueba comandios")
-        interaction.reply('comandos prueba');
-        try {
-            for (i = 0; i < 10;) {
-                setTimeout(() => {
-                    client.users.fetch('268003611173191681', false).then((user) => {
-                        user.send('wn wn wn ');
-                    });
-                }, 2000);
-                i++
+    let args = message.content.substring(prefix.length).split(" ");
+    console.log(args)
+    switch (args[1]) {
+        case 'algo':
+            message.channel.send('que pasa larva')
+            break;
+        case 'play':
+            if (!message.member.voice?.channel) {
+                return message.channel.send('Conectate a un canal de voz')
             }
-
-
-        } catch (error) {
-            console.error("error en el comando ")
-        }
-    }
-
-
-    if (commandName === 'lushoql') {
-        console.log("comando lusho ql")
-        for (i = 0; i < 10;) {
+            const connection = joinVoiceChannel({
+                channelId: message.member.voice.channelId,
+                guildId: message.guildId,
+                adapterCreator: message.guild.voiceAdapterCreator
+            })
             try {
-
-                setTimeout(() => {
-                    client.users.fetch('387294595479896065', false).then((user) => {
-                        user.send('weta qlo');
-                    });
-                }, 2000);
-
-
+                url = args[2]
+                cancion = await ytdl(url, {
+                    filter: 'audioonly'
+                })
+                info = await ytdl.getInfo(url)
+                message.channel.send(`reproduciendo ${info.videoDetails.title}`)
+                const player = createAudioPlayer();
+                connection.subscribe(player);
+                let resource = createAudioResource(cancion);
+                player.play(resource)
+                player.on(AudioPlayerStatus.Playing, () => {
+                    console.log("reproduciendo la cancion")
+                });
+                player.on(AudioPlayerStatus.Idle, () => {
+                    console.log("termino la cancion")
+                    message.channel.send(`termino la cancion, pone otra`)
+                    connection.disconnect();
+                });
             } catch (error) {
-                console.error("error en el comando")
+                console.error(error)
+                return
             }
-            i++
-        }
-
-
-        await interaction.reply('weta qlo');
-    }
-    if (commandName === 'johnql') {
-        try {
-            await client.users.fetch('447586866317885441', false).then((user) => {
-                user.send('guaton ql');
-            });
-        } catch (error) {
-            console.error("error en el comando")
-        }
-
-        await interaction.reply('guaton ql');
+            break
+        default:
+            break;
     }
 });
 

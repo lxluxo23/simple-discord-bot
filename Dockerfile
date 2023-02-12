9ccfb3b21aa4d0d1a3073ea6164181e035ed9cf1
Luis cespedes <lxluxo23@gmail.com>
2023-02-11 21:29:16 -0300

    colas funcionando

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM  node:16-bullseye-slim
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
+RUN apt update
+RUN apt install ffmpeg -y 
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -20,20 +20,19 @@ const {
     createAudioResource
 } = require('@discordjs/voice');
 const token = process.env.TOKEN
+
+let cola = []
+
 client.on('ready', () => {
     console.log(`Logueado en el bot  ${client.user.tag}!`)
     client.user.setActivity('pepillo GOD', { type: ActivityType.Playing });
-
 });
 
 client.on('interactionCreate', async interaction => {
-
-    // algun dia llenare esto
-
-
+    // algun día llenaré esto
 });
-client.on('messageCreate', async (message) => {
 
+client.on('messageCreate', async (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) {
         return
     }
@@ -43,7 +42,7 @@ client.on('messageCreate', async (message) => {
         case 'algo':
             message.channel.send('que pasa larva')
             break;
-        case 'play':
+        case 'p':
             if (!message.member.voice?.channel) {
                 return message.channel.send('Conectate a un canal de voz')
             }
@@ -54,34 +53,12 @@ client.on('messageCreate', async (message) => {
             })
             try {
                 url = args[2]
-                cancion = await ytdl(url, {
-                    filter: 'audioonly'
-                })
-                info = await ytdl.getInfo(url)
-                message.channel.send(`reproduciendo ${info.videoDetails.title}`)
-                const writeStream = fs.createWriteStream('cancion.mp3')
-
-                cancion.pipe(writeStream)
-                // Esperar que descargue la cancion 
-                await new Promise((resolve, reject) => {
-                    writeStream.on('close', resolve)
-                    writeStream.on('error', reject)
-                })
-                let resource = createAudioResource("./cancion.mp3")
-                const player = createAudioPlayer();
-                connection.subscribe(player);
-                player.play(resource)
-                player.on(AudioPlayerStatus.Playing, () => {
-                    console.log("reproduciendo la cancion")
-                });
-                player.on(AudioPlayerStatus.Idle, () => {
-                    console.log("termino la cancion")
-                    message.channel.send(`termino la cancion, pone otra`)
-                    connection.disconnect();
-                    fs.rm("./cancion.mp3", () => {
-                        console.log("cancion removida")
-                    })
-                });
+                cola.push(url)
+                if (cola.length === 1) {
+                    playCancion(cola[0], connection, message.channel)
+                } else {
+                    message.channel.send(`Agregada a la cola: ${url}`)
+                }
             } catch (error) {
                 console.error(error)
                 return
@@ -93,3 +70,43 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(token)
+
+async function playCancion(url, connection, channel) {
+    try {
+        cancion = await ytdl(url, {
+            filter: 'audioonly'
+        })
+        info = await ytdl.getInfo(url)
+        console.log(`reproduciendo ${info.videoDetails.title}`)
+        channel.send(`reproduciendo ${info.videoDetails.title}`)
+        const writeStream = fs.createWriteStream('cancion.mp3')
+        cancion.pipe(writeStream)
+        await new Promise((resolve, reject) => {
+            writeStream.on('close', resolve)
+            writeStream.on('error', reject)
+        })
+        let resource = createAudioResource("./cancion.mp3")
+        const player = createAudioPlayer();
+        connection.subscribe(player);
+        player.play(resource)
+        player.on(AudioPlayerStatus.Playing, () => {
+            console.log("reproduciendo la cancion")
+        });
+        player.on(AudioPlayerStatus.Idle, () => {
+            console.log("termino la cancion")
+            // message.channel.send(`termino la cancion, pone otra`)
+            fs.rm("./cancion.mp3", () => {
+                console.log("cancion removida")
+            })
+            cola.shift();
+            if (cola.length > 0) {
+                playCancion(cola[0], connection, channel);
+            } else {
+                channel.send('La cola de canciones ha terminado.');
+            }
+        });
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
